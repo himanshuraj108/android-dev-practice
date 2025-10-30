0ca33eb09f78c3976c253f639eb4184cfe256743
Himanshu <himanshuraj48512@gmail.com>
2025-10-30 13:37:48 +0530

    Toast added

--- app/src/main/java/com/example/aclass/NewForm.kt
+++ app/src/main/java/com/example/aclass/NewForm.kt
@@ -1,6 +1,9 @@
 package com.example.aclass
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,17 @@ import androidx.core.view.WindowInsetsCompat
 class NewForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_new_form)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        var registerBottom: Button = findViewById(R.id.register)
+        var resetButton: Button = findViewById(R.id.reset)
+
+        registerBottom.setOnClickListener {
+            Toast.makeText(this,"Register Success", Toast.LENGTH_LONG).show()
+        }
+
+        resetButton.setOnClickListener {
+            Toast.makeText(this,"Reset Success",Toast.LENGTH_LONG).show()
         }
     }
 }
